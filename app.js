3862ace86d65a46c9524c166092857afff4e3d4f
Zendy <nguyenduc.051194@gmail.com>
2023-01-16 17:21:38 +0700

    Create router

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send(`Task manager app`);
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(PORT, console.log * `Server is listening on port ${PORT}...`);
